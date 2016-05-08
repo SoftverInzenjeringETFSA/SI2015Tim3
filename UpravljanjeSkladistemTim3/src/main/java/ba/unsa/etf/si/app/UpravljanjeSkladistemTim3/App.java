b8a5d6cc3facc03c20abfd430af9277abad6d06a
mirzetb <mbrkic3@etf.unsa.ba>
2016-05-08 21:41:53 +0200

    class & hibernate

--- UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/App.java
+++ UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/App.java
@@ -4,7 +4,7 @@ package ba.unsa.etf.si.app.UpravljanjeSkladistemTim3;
  * Hello world!
  *
  */
-import ba.unsa.etf.si.app.UpravljanjeSkladistemTim3.BLL.*;
+//import ba.unsa.etf.si.app.UpravljanjeSkladistemTim3.BLL.*;
 import ba.unsa.etf.si.app.UpravljanjeSkladistemTim3.DAL.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -13,22 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        Session session = HibernateUtil.getSessionFactory().openSession(); 
-        dodajPoslovnogPartnera(session);
-        session.close();
+       
     }
-
-	private static void dodajPoslovnogPartnera(Session session) {
-		Transaction t = session.beginTransaction();
-		PoslovniPartner pp = new PoslovniPartner();
-		
-		pp.setAdresa("kdkadk");
-		pp.setJIB("55335");
-		pp.setNaziv("firma");
-		
-		Long id = (Long) session.save(pp);
-		System.out.println("Dodan pp sa IDom" + id);
-		t.commit();
-	}
 }
--- UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/DAL/Dokument.java
+++ UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/DAL/Dokument.java
@@ -3,6 +3,8 @@ package ba.unsa.etf.si.app.UpravljanjeSkladistemTim3.DAL;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.*;
+import javax.persistence.*;
 
 public abstract class Dokument implements Serializable {
 	long id;
@@ -29,7 +31,6 @@ public abstract class Dokument implements Serializable {
 	public void setVrijednost(double vrijednost) {
 		this.vrijednost = vrijednost;
 	}
-
 	public List<StavkaDokumenta> get_stavke() {
 		return _stavke;
 	}
@@ -37,9 +38,18 @@ public abstract class Dokument implements Serializable {
 	public void set_stavke(List<StavkaDokumenta> _stavke) {
 		this._stavke = _stavke;
 	}
+
+	public Uposlenik get_kreirao() {
+		return _kreirao;
+	}
+
+	public void set_kreirao(Uposlenik _kreirao) {
+		this._kreirao = _kreirao;
+	}
 	
 	Date datum;
 	double vrijednost;
+	Uposlenik _kreirao;
 	List<StavkaDokumenta> _stavke;
 	
 	public Dokument() {}
