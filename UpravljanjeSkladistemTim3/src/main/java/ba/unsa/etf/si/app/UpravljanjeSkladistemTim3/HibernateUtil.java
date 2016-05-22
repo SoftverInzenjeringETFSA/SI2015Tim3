40f85fb501a50ae0fc9981708d877c6b2e37756d
Nermin Boja <nboja1@etf.unsa.ba>
2016-05-22 21:53:17 +0200

    Priprema za instalacionu proceduru

--- UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/HibernateUtil.java
+++ UpravljanjeSkladistemTim3/src/main/java/ba/unsa/etf/si/app/UpravljanjeSkladistemTim3/HibernateUtil.java
@@ -1,25 +1,38 @@
 package ba.unsa.etf.si.app.UpravljanjeSkladistemTim3;
 
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.imageio.spi.ServiceRegistry;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.*;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	  private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static final SessionFactory sessionFactory = buildSessionFactory();
 
-	    private static SessionFactory buildSessionFactory() {
-	        try {
-	            // Create the SessionFactory from hibernate.cfg.xml
-	            return new Configuration().configure().buildSessionFactory();
-	        }
-	        catch (Exception ex) {
-	            // Make sure you log the exception, as it might be swallowed
-	            System.err.println("Initial SessionFactory creation failed." + ex);
-	            throw new ExceptionInInitializerError(ex);
-	        }
-	    }
+	private static SessionFactory buildSessionFactory() {
+		try {
+			java.util.Properties properties = new Properties();
+			properties.load(new FileInputStream("db.properties"));
+			// Create the SessionFactory from hibernate.cfg.xml
+			return new Configuration().addProperties(properties).configure().buildSessionFactory();
+		} catch (Exception ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+
 }
+
+
